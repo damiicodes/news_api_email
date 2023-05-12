faab6fdeee6865c18ec54cc1dd1b6d26090eae39
Dami <dami.fajinmi@hotmail.com>
2023-05-12 13:50:06 +0100

    created email body

--- main.py
+++ main.py
@@ -9,9 +9,21 @@ request = requests.get(url)
 # Get dictionary with data
 content = request.json()
 
+# for article in content['results']:
+#     if article['title'] is None or article['description'] is None\
+#             or article['link'] is None:
+#           print(article)
+
 # Access article title and description
+body = ''
 for article in content['results']:
-    print(article['title'])
-    print(article['description'])
+      if article['description'] is not None:
+            body = body + article['title'] + '\n' \
+            + article['description'] \
+            + '\n' + article['link'] + 2*'\n'
+
+print(body)
+
+
 
 
